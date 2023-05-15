c4722f8fbd5c40aa8749e846c681178f7447009b
imrismaa <rismasaputri@mail.ugm.ac.id>
2023-05-15 21:58:32 +0700

    Kelas Tabung Final

--- src/bangun3D/Tabung.java
+++ src/bangun3D/Tabung.java
@@ -1,5 +1,45 @@
 package bangun3D;
 
 public class Tabung {
-    //TODO: Belum Jadi Sama Sekali (Hapus yang tidak perlu)
+    private double phi = 22.0/7;
+    private double r;
+    private double tinggi;
+
+    public Tabung(double r, double tinggi) {
+        this.r = r;
+        this.tinggi = tinggi;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    private double getLuas() {
+        return 2*getLuasAlas() + 2*phi*r*tinggi;
+    }
+
+    private double getLuasAlas() {
+        return phi*r*r;
+    }
+
+    public double getVolume() {
+        return phi*r*r*tinggi;
+    }
+
+    @Override
+    public String toString() {
+        return "Tabung{" + "r=" + r + "tinggi=" + tinggi + '}';
+    }
 }
