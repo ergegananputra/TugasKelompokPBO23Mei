d5a8a27cfbf6b72e00bc3d92f3cbb2415b6afbce
ergegananputra <ergegananputra@gmail.com>
2023-05-15 20:39:38 +0700

    Kelas Persegi V1

--- src/bangun2D/Persegi.java
+++ src/bangun2D/Persegi.java
@@ -2,4 +2,35 @@ package bangun2D;
 
 public class Persegi {
     //TODO: Belum Jadi Sama Sekali (Hapus yang tidak perlu)
+
+    // Atributes
+    private double sisi;
+    public Persegi(double sisi){
+        this.sisi = sisi;
+    }
+
+    // Getter & Setter
+    public double getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(double sisi) {
+        this.sisi = sisi;
+    }
+
+    // Methods
+    public double getLuas(){
+        return sisi*sisi;
+    }
+
+    public double getKeliling(){
+        return 4*sisi;
+    }
+
+    @Override
+    public String toString() {
+        return "Persegi{" +
+                "sisi=" + sisi +
+                '}';
+    }
 }
