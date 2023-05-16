aecb0a9173dc001e57699ab3d8e7ad4289354374
ergegananputra <ergegananputra@gmail.com>
2023-05-16 17:00:49 +0700

    Add Main class test Case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import bangun2D.*;
+import bangun3D.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // bangun 2D
+        Persegi persegi = new Persegi(10);
+        PersegiPanjang persegiPanjang = new PersegiPanjang(5, 10);
+        SegitigaSamaKaki segitigaSamaKaki = new SegitigaSamaKaki();
+        SegitigaSamaSisi segitigaSamaSisi = new SegitigaSamaSisi();
+        SegitigaSiku segitigaSiku = new SegitigaSiku();
+
+        List<Object> bangun2D = Arrays.asList(persegi, persegiPanjang, segitigaSamaKaki, segitigaSamaSisi, segitigaSiku);
+        System.out.println("=== Bangun 2D ===");
+        for (Object bangun: bangun2D) {
+            String msg = bangun.toString();
+            if (!msg.startsWith("bangun2D")){
+                System.out.println(msg);
+            } else {
+                System.out.println("[ Unimplemented Error! ] \t" + bangun.getClass());
+            }
+        }
+
+        // Divider
+        System.out.println("\n");
+
+        // bangun 3D
+        Balok balok = new Balok(10, 10, 10);
+        Kerucut kerucut = new Kerucut();
+        Kubus kubus = new Kubus(10);
+        Limas limas = new Limas();
+        Prisma prisma = new Prisma();
+        Tabung tabung = new Tabung(7, 10);
+
+        List<Object> bangun3D = Arrays.asList(balok, kerucut, kubus, limas, prisma, tabung);
+        System.out.println("=== Bangun 3D ===");
+        for (Object bangun: bangun3D) {
+            String msg = bangun.toString();
+            if (!msg.startsWith("bangun3D")){
+                System.out.println(msg);
+            } else {
+                System.out.println("[ Unimplemented Error! ] \t" + bangun.getClass());
+            }
+        }
+
+
+
     }
 }
