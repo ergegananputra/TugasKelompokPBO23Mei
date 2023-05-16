1aa13070fe4eb18f105b9ebacf909c13ef08eae4
Hailpanda <Vikovanesta@gmail.com>
2023-05-16 17:16:51 +0700

    sikusikusikusiku

--- src/bangun2D/SegitigaSiku.java
+++ src/bangun2D/SegitigaSiku.java
@@ -1,5 +1,31 @@
 package bangun2D;
 
-public class SegitigaSiku {
-    //TODO: Belum Jadi Sama Sekali (Hapus yang tidak perlu)
+public class SegitigaSiku extends Segitiga {
+    //Atributes
+    private double sisiMiring;
+
+    //Constructor
+    public SegitigaSiku(double alas, double tinggi, double sisiMiring){
+        super(alas, tinggi);
+        this.sisiMiring = sisiMiring;   
+    }
+
+    //Methods
+    @Override
+    public double getKeliling() {
+        return getAlas() + getTinggi() + getSisiMiring();
+    }
+
+    public String toString(){
+        return "SegitigaSiku{" + "alas=" + getAlas() +"' tinggi=" + getTinggi() + "' sisiMiring=" + getSisiMiring() +"}";
+    }
+
+    //Getter Setter
+    public double getSisiMiring() {
+        return this.sisiMiring;
+    }
+
+    public void setSisiMiring(double sisiMiring) {
+        this.sisiMiring = sisiMiring;
+    }
 }
