3bb787062f5f6ae4d65bcfaa6f3fe82207bb14fe
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-16 18:23:55 +0700

    Add PrismaSegitiga body

--- src/bangun3D/Prisma.java
+++
@@ -1,5 +0,0 @@
-package bangun3D;
-
-public class Prisma {
-    //TODO: Belum Jadi Sama Sekali (Hapus yang tidak perlu)
-}
---
+++ src/bangun3D/PrismaSegitiga.java
@@ -0,0 +1,55 @@
+package bangun3D;
+
+import bangun2D.Segitiga;
+import bangun2D.SegitigaSiku;
+import bangun2D.SegitigaSamaKaki;
+import bangun2D.SegitigaSamaSisi;
+
+public class PrismaSegitiga {
+    // Attributes
+    private Segitiga alas;
+    private double tinggi;
+
+    // Constructor
+    public PrismaSegitiga(Segitiga alas, double tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    // Setter & Getter
+    public Segitiga getAlas() {
+        return alas;
+    }
+    public void setAlas(Segitiga alas) {
+        this.alas = alas;
+    }
+    public double getTinggi() {
+        return tinggi;
+    }
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    // Method
+    public double getVolume(){
+        return alas.getAlas() * tinggi;
+    }
+    public double getLuas(){
+        return 2 * alas.getLuas() + alas.getKeliling() * tinggi;
+    }
+    public String getDetailAlas(){
+        if (alas instanceof SegitigaSiku){
+            return "Segitiga Siku";
+        } else if (alas instanceof SegitigaSamaKaki) {
+            return "Segitiga Sama Kaki";
+        } else if (alas instanceof SegitigaSamaSisi) {
+            return "Segitiga Sama Sisi";
+        } else{
+            return "Segitiga";
+        }
+    }
+    @Override
+    public String toString() {
+        return "PrismaSegitiga{" + "alas=" + alas + ", tinggi=" + tinggi + '}';
+    }
+}
