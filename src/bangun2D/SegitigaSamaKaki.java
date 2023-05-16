c0023b91eb1dc62924e6b0544572ddfe18393eba
Ayaya62 <andhikayogapratama@mail.ugm.ac.id>
2023-05-16 17:26:42 +0700

    sama kaki

--- src/bangun2D/SegitigaSamaKaki.java
+++ src/bangun2D/SegitigaSamaKaki.java
@@ -1,5 +1,23 @@
 package bangun2D;
+import java.lang.Math;
 
-public class SegitigaSamaKaki {
-    //TODO: Belum Jadi Sama Sekali (Hapus yang tidak perlu)
+public class SegitigaSamaKaki extends Segitiga{
+    public SegitigaSamaKaki(double alas, double tinggi) {
+        super(alas, tinggi);
+    }
+
+    @Override
+    public double getKeliling() {
+        double sisiMiring = Math.sqrt(Math.pow(getAlas(), 2) + Math.pow(getTinggi(), 2));
+        return sisiMiring * 2 + getAlas();
+    }
+
+    public double getLuas() {
+        return getAlas() / 2 * getTinggi();
+    }
+
+    @Override
+    public String toString() {
+        return "SegitigaSamaKaki{" + "alas" + getAlas() +" lebar=" + getTinggi() + "}";
+    }
 }
