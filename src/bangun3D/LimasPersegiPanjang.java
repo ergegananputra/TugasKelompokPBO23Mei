ac2abf6bc208800f4032291a4302d02174262d79
Amanda F <44936062+nekkuzuria@users.noreply.github.com>
2023-05-17 11:25:01 +0700

    Create LimasPersegiPanjang.java

---
+++ src/bangun3D/LimasPersegiPanjang.java
@@ -0,0 +1,32 @@
+package bangun3D;
+
+import bangun2D.PersegiPanjang;
+
+public class LimasPersegiPanjang extends Limas {
+    private PersegiPanjang alas;
+
+    public LimasPersegiPanjang(double tinggi, PersegiPanjang alas) {
+        super(tinggi);
+        this.alas = alas;
+    }
+
+
+    @Override
+    public double getLuasAlas() {
+        return alas.getLuas();
+    }
+
+    @Override
+    public double getVolume() {
+        return 1/3 * alas.getLuas() * getTinggi();
+    }
+
+    @Override
+    public String toString() {
+        return "LimasPersegiPanjang{" +
+                "panjang alas = " + alas.getPanjang() +
+                "lebar alas = " + alas.getLebar() +
+                " tinggi = " + getTinggi() +
+                '}';
+    }
+}
