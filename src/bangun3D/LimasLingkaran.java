762bd9459470cadb0426b94861228390451aa3b0
Amanda F <44936062+nekkuzuria@users.noreply.github.com>
2023-05-17 11:25:19 +0700

    Create LimasLingkaran.java

---
+++ src/bangun3D/LimasLingkaran.java
@@ -0,0 +1,31 @@
+package bangun3D;
+
+import bangun2D.Lingkaran;
+
+public class LimasLingkaran extends Limas {
+    private Lingkaran alas;
+
+    public LimasLingkaran(double tinggi, Lingkaran alas) {
+        super(tinggi);
+        this.alas = alas;
+    }
+
+
+    @Override
+    public double getLuasAlas() {
+        return alas.getLuas();
+    }
+
+    @Override
+    public double getVolume() {
+        return 1/3 * alas.getLuas() * getTinggi();
+    }
+
+    @Override
+    public String toString() {
+        return "LimasLingkaran{" +
+                "R alas = " + alas.getR() +
+                " tinggi = " + getTinggi() +
+                '}';
+    }
+}
