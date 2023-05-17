872a9c34a916aa338c1d25ddb6891f9ed1867dcc
Amanda F <44936062+nekkuzuria@users.noreply.github.com>
2023-05-17 11:54:42 +0700

    Create LimasSegitigaSiku.java

---
+++ src/bangun3D/LimasSegitigaSiku.java
@@ -0,0 +1,33 @@
+package bangun3D;
+
+import bangun2D.SegitigaSiku;
+
+public class LimasSegitigaSiku extends Limas{
+    private SegitigaSiku alas;
+
+    public LimasSegitigaSiku(double tinggi, SegitigaSiku alas) {
+        super(tinggi);
+        this.alas = alas;
+    }
+
+
+    @Override
+    public double getLuasAlas() {
+        return alas.getLuas();
+    }
+
+    @Override
+    public double getVolume() {
+        return 1/3 * alas.getLuas() * getTinggi();
+    }
+
+    @Override
+    public String toString() {
+        return "LimasSegitigaSamaKaki{" +
+                "a alas = " + alas.getAlas() +
+                " t alas = " + alas.getTinggi() +
+                " tinggi = " + getTinggi() +
+                '}';
+    }
+}
+
