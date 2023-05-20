450d57a58e2ae5eec98823fa0b51eabcdb50a139
mine1808 <117977364+mine1808@users.noreply.github.com>
2023-05-20 23:11:08 +0700

    Update MainPrismaSlide60.java

--- src/TestingFacilities/MainFromSlides/MainPrismaSlide60.java
+++ src/TestingFacilities/MainFromSlides/MainPrismaSlide60.java
@@ -4,10 +4,18 @@ import bangun3D.PrismaSegitiga;
 
 public class MainPrismaSlide60 {
     public static void run(){
+        try {
         PrismaSegitiga prisma = new PrismaSegitiga(null, 10);
         System.out.println(prisma);
         System.out.println(prisma.getVolume());
         System.out.println(prisma.getDetailAlas());
         System.out.println("----------------------------");
+        } catch (NullPointerException e) {
+          System.out.println("Error: " + e.getMessage());
+    }
+}
+
+     public static void main(String[] args) {
+        run();
     }
 }
