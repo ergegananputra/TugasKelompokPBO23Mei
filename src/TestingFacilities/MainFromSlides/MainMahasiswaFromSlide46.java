7e194a09f371064c8f82173febcb63db07bdc341
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-23 14:44:51 +0700

    New class: MainMahasiswaFromSlide46.java

---
+++ src/TestingFacilities/MainFromSlides/MainMahasiswaFromSlide46.java
@@ -0,0 +1,31 @@
+package TestingFacilities.MainFromSlides;
+
+import Paket.Mahasiswa;
+
+public class MainMahasiswaFromSlide46 {
+    public static void main(){
+        // Error karena atribut private
+//        System.out.println(Mahasiswa.prodi);
+        // Error karena atribut non-static
+//        System.out.println(Mahasiswa.nama);
+
+        Mahasiswa st1 = new Mahasiswa("Mr X satu");
+        System.out.println(st1.getProdi());
+        System.out.println(st1.getNama());
+
+        Mahasiswa st2 = new Mahasiswa("Mr X dua");
+        System.out.println(st2.getProdi());
+        System.out.println(st2.getNama());
+
+        st1.setProdi("Teknologi RPL");
+        st1.setNama("Agus");
+        st2.setProdi("Teknologi Rekayasa Perangkat Lunak");
+        st2.setNama("Budi");
+
+        System.out.println(st1.getProdi());
+        System.out.println(st1.getNama());
+
+        System.out.println(st2.getProdi());
+        System.out.println(st2.getNama());
+    }
+}
