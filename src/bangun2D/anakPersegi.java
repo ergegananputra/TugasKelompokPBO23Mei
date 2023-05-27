1bdf8612002798f2b40d128d9a70a63906047ee4
ergegananputra <ergegananputra@gmail.com>
2023-05-27 09:51:27 +0700

    add anakPersegi for experiment from slide 51

---
+++ src/bangun2D/anakPersegi.java
@@ -0,0 +1,12 @@
+package bangun2D;
+
+public class anakPersegi extends Persegi{
+    public anakPersegi(double sisi) {
+        super(sisi);
+    }
+
+    public double contoh2(){
+        return getSisi();
+    }
+
+}
